7828a587c83f6d7f34f64fcb61c1964cadd94e30
JettPy <55996004+JettPy@users.noreply.github.com>
2019-11-08 00:49:38 +0300

    Create  	isometry.cpp

---
+++ lib/isometry.cpp
@@ -0,0 +1,26 @@
+#include "isometry.h"
+
+template <typename C3V, typename C2V>
+Isometry<C3V, C2V>::Isometry(double angle, double scale)
+{
+}
+
+template <typename C3V, typename C2V>
+Isometry<C3V, C2V>::~Isometry()
+{
+}
+
+template <typename C3V, typename C2V>
+void Isometry<C3V, C2V>::TransformCoordinates(std::vector<C3V> coordinates, std::vector<C2V> flat_coordinates)
+{
+}
+
+template <typename C3V, typename C2V>
+void Isometry<C3V, C2V>::Zooming(double scale);
+{
+}
+
+template <typename C3V, typename C2V>
+void Isometry<C3V, C2V>::Scaling(double scale)
+{
+}
