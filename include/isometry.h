c102669935898e4e7b4165cb3fd3aa86e4c7770c
JettPy <55996004+JettPy@users.noreply.github.com>
2019-11-08 15:36:14 +0300

    Update isometry.h
    Добавлены пробелы между <> и исправлена опечатка в namespace

--- include/isometry.h
+++ include/isometry.h
@@ -3,17 +3,17 @@
 
 #include <vector>
 
-using namespace Polaris;
+namespace Polaris;
 {
 	
-template <typename C3V, typename C2V> // C3V - трехмерные координаты, C2V - двухмерные координаты
+template < typename C3V, typename C2V > // C3V - трехмерные координаты, C2V - двухмерные координаты
 class Isometry
 {
 public:
 	Isometry( double angle, double scale ); // Инициализация конструктора углом поворота и коэффицентом масштабирования
 	~Isometry();
 
-	void TransformCoordinates( std::vector<C3V> & coordinates, std::vector<C2V> & flat_coordinates ); // Трехмерные координаы в двухмерные
+	void TransformCoordinates( std::vector< C3V > & coordinates, std::vector< C2V > & flat_coordinates ); // Трехмерные координаы в двухмерные
 	void Zooming( double scale ); // Изменение коэффицента масштабирования
 	void Scaling( double scale ); // Изменение угла поворота
 
