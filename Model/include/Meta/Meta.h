a326e5c4c7c0f59b981b819e06d9264cb9d41a22
MBtsky <9250499222@mail.ru>
2019-11-12 01:29:20 +0300

    Meta struct with docs

--- Model/include/Meta/Meta.h
+++ Model/include/Meta/Meta.h
@@ -3,10 +3,26 @@
 
 namespace Polaris
 {
-class Meta
+// Type used in GraphNode for id_ field;
+using Id = size_t;
+/******************************************************************************
+ * Meta structure is used to store additional information about GraphNode.
+ * It is separated from GraphNode to increase flexibility
+ * between project parts.
+ *****************************************************************************/
+struct Meta
 {
-
+public:
+    Id graph_node_id;
+    size_t room_number;
+    double x, y;
+public:
+    explicit Meta( Id a_graph_node_id, size_t a_room_number = 0,
+            double a_x = 0, double a_y = 0 )
+    : graph_node_id( a_graph_node_id ),
+    room_number( a_room_number ),
+    x( a_x ), y( a_y ){};
 };
-}//namespace Polaris
+} //namespace Polaris
 
 #endif //GRAPHSEARCH_META_HPP
