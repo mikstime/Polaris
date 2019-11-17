ef6a3f893f649ab6c6727e0e0944322108e4165b
MBtsky <9250499222@mail.ru>
2019-11-17 12:12:11 +0300

    model interface tests and mocks

--- Model/lib/ModelObserver/model_observer_tests.cpp
+++ Model/lib/ModelObserver/model_observer_tests.cpp
@@ -1,1 +1,35 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "include/Meta/Meta.h"
+#include "include/ModelObserver/ModelObserver.h"
+#include "include/GraphNode/GraphNode.h"
+#include "include/GraphConnection/GraphConnection.h"
+#include "include/ModelSubscriber/ModelSubscriber.h"
+using namespace Polaris;
+class MockModelObserver: public ModelObserver
+{
+public:
+    MOCK_METHOD(
+    void, Subscribe,( const ModelSubscriber & subscriber ), ()
+    );
+    MOCK_METHOD(
+    void, unSubscribe,( const ModelSubscriber & subscriber ), ()
+    );
+    MOCK_METHOD(
+    void, MetaChanged,( const Meta & newMeta, const Model & modelState ), ()
+    );
+    MOCK_METHOD(
+    void, ConnectionAdded,( const GraphNode & firstNode,
+                          const GraphNode & lastNode,
+                          const Model & modelState ), ()
+    );
+    MOCK_METHOD(
+    void, ConnectionRemoved,( const GraphNode & firstNode,
+                            const GraphNode & lastNode,
+                            const Model & modelState ), ()
+    );
+    MOCK_METHOD(
+    void, NodeAdded,( const GraphNode & node, const Model & modelState ), ());
+    MOCK_METHOD(
+    void, NodeRemoved,( const GraphNode & node, const Model & modelState ), ());
+};
