bd80326f34ff8cd93a5b394b4f2243607e020adb
JettPy <55996004+JettPy@users.noreply.github.com>
2019-11-17 12:26:09 +0300

    Added namespace & codstyle

--- network/include/network.h
+++ network/include/network.h
@@ -12,106 +12,124 @@
 #include <memory>
 #include <vector>
 
-class DataHandlerImple;
-
-class DataHandler
+namespace Polaris
 {
-public:
-    DataHandler( DataHandlerImple * ptr ) : ptr_imp( ptr ) {}
-    DataHandler() {}
-    virtual ~DataHandler() = default;
-    virtual void Read() = 0;
-    virtual void Write() = 0;
+    class DataHandlerImple;
 
-protected:
-    DataHandlerImple * ptr_imp;
+    class DataHandler
+    {
+    public:
+        DataHandler( DataHandlerImple * ptr ) : ptr_imp( ptr ) {}
+        DataHandler() {}
+        virtual ~DataHandler() = default;
+        virtual void Read() = 0;
+        virtual void Write() = 0;
 
-};
+    protected:
+        DataHandlerImple * ptr_imp;
 
-class DataHandlerImple
-{
-public:
-    virtual ~DataHandlerImple() {}
-    virtual void Open() = 0;
-    virtual void Close() = 0;
+    };
 
-};
+    class DataHandlerImple
+    {
+    public:
+        virtual ~DataHandlerImple() {}
+        virtual void Open() = 0;
+        virtual void Close() = 0;
 
-class Network : public DataHandlerImple
-{
-public:
-    void Open() override;
-    void Close() override;
+    };
 
-};
+    class Network : public DataHandlerImple
+    {
+    public:
+        void Open() override;
+        void Close() override;
 
-class Localhost : public DataHandlerImple
-{
-public:
-    void Open() override;
-    void Close() override;
+    };
 
-};
+    class Localhost : public DataHandlerImple
+    {
+    public:
+        void Open() override;
+        void Close() override;
 
-class Reader : public DataHandler
-{
-public:
-    Reader( DataHandlerImple * ptr ) : DataHandler( ptr ) {}
-    void Read() override;
-};
+    };
 
-class Writer : public DataHandler
-{
-public:
-    Writer( DataHandlerImple * ptr ) : DataHandler( ptr ) {}
-    void Write() override;
-};
+    class Reader : public DataHandler
+    {
+    public:
+        Reader( DataHandlerImple * ptr ) : DataHandler( ptr ) {}
+        void Read() override;
+        
+    };
 
-enum class Client_state_t: uint8_t { WANT_READ, WANT_WRITE };
+    class Writer : public DataHandler
+    {
+    public:
+        Writer( DataHandlerImple * ptr ) : DataHandler( ptr ) {}
+        void Write() override;
+        
+    };
 
-class Engine {
-    int m_Listener;
-    int CreateServerSocket( uint32_t port, uint32_t listen_queue_size );
-public:
-    explicit Engine( int port ) : m_Listener( port ) {}
-    virtual void Run() = 0;
-    int Listener() const { return m_Listener; }
-};
+    enum class Client_state_t : uint8_t { WANT_READ, WANT_WRITE };
 
-class Client {
-    int _sd;
-public:
-    Client(int _sd) : _sd(_sd) {}
-    ~Client()
-    { if (_sd > 0) close(_sd); }
-public:
-    void Connect(const std::string &host, int port);
-    void Send(const std::string &s);
-    std::string Recv();
-    void SetRcvTimeout(int sec, int microsec);
-    void SetNonBlocked(bool opt);
-    bool DataAvailable();
-};
+    class Engine
+    {
+    public:
+        explicit Engine( int port ) : m_Listener( port ) {}
+        virtual void Run() = 0;
+        int Listener() const { return m_Listener; }
+        
+    private:
+        int m_Listener;
+        int CreateServerSocket( uint32_t port, uint32_t listen_queue_size );
+        
+    };
 
-class PollEngine: public Engine {
-    std::vector<Client> m_Clients;
-    void EventLoop();
-public:
-    explicit PollEngine(int port): Engine(port) {}
-    void Run() override;
-    int Prepare(struct pollfd *fds);
-};
+    class Client
+    {
+    public:
+        Client( int _sd ) : _sd(_ sd ) {}
+        ~Client()
+        { if ( _sd > 0 ) close( _sd ); }
+        void Connect( const std::string & host, int port );
+        void Send( const std::string & s );
+        std::string Recv();
+        void SetRcvTimeout( int sec, int microsec );
+        void SetNonBlocked( bool opt );
+        bool DataAvailable();
+        
+    private:
+            int _sd;
+        
+    };
 
-class Server
-{
-public:
-    Server() : m_Sd(-1) {}
-    ~Server() { if (m_Sd > 0) close(m_Sd); }
-    void CreateServerSocket(uint32_t port, uint32_t queue_size);
-    std::shared_ptr<Client> Accept() ;
-    void SetNonBlocked(bool opt);
-private:
-    int m_Sd;
-};
+    class PollEngine: public Engine
+    {
+    public:
+        explicit PollEngine(int port): Engine(port) {}
+        void Run() override;
+        int Prepare(struct pollfd *fds);
+        
+    private:
+        std::vector< Client > m_Clients;
+        void EventLoop();
+        
+    };
+
+    class Server
+    {
+    public:
+        Server() : m_Sd( -1 ) {}
+        ~Server() { if ( m_Sd > 0 ) close( m_Sd ); }
+        void CreateServerSocket( uint32_t port, uint32_t queue_size );
+        std::shared_ptr< Client > Accept() ;
+        void SetNonBlocked( bool opt );
+        
+    private:
+        int m_Sd;
+        
+    };
+}
 
 #endif //NETWORK_NETWORK_H
