73ec61ab8104ebb83758695428dd14311a7cc70e
Michael Balitsky <35119841+mikstime@users.noreply.github.com>
2019-11-30 10:53:52 +0300

    Delete graph_connection_tests.cpp

--- Model/lib/GraphConnection/graph_connection_tests.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "include/GraphConnection/GraphConnection.h"
-#include <catch2/catch.hpp>
-
-TEST_CASE( "ID generation test", "[Polaris::GraphConnection]" ) {
-    auto ge = Polaris::GraphConnection();
-    REQUIRE( ge.getId() == 0 );
-}
