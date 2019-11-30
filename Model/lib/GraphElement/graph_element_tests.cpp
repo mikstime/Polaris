062e4bf69550dd72c31db2c1e8729312fe3d02fc
Michael Balitsky <35119841+mikstime@users.noreply.github.com>
2019-11-30 10:53:28 +0300

    Delete graph_element_tests.cpp

--- Model/lib/GraphElement/graph_element_tests.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "include/GraphElement/GraphElement.h"
-#include <catch2/catch.hpp>
-
-TEST_CASE( "ID generation test", "[GraphElement]" ) {
-    auto ge = Polaris::GraphElement();
-    REQUIRE(ge.getId() == 0);
-}
