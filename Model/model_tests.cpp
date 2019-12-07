902f018c93a4a5ecc54cf307e266affd8d3f3291
Michael Balitsky <35119841+mikstime@users.noreply.github.com>
2019-12-07 12:07:09 +0300

    Search model (#25)
    * cmake updated
    
    * Model subscriber class with documentation
    
    * Meta struct with docs
    
    * Model structure with docs.
    
    * Meta constructor default arg changed
    
    * ModelInterface with docs
    
    * Model observer docs
    
    * class replaced by struct. All logic removed
    
    * Graph structure with docs.
    
    * ModelProxy with docs
    
    * headers added
    
    * GraphInterface with docs
    
    * Connection Params added
    
    * Mocks for GraphInnterface
    
    * Minor changes
    
    * model interface tests and mocks
    
    * ModelObserver specs updated
    
    * ModelSubscriber specs updated
    
    * docs updated
    
    * Model observer implementation.
    Model subscriber updated.
    
    * virtual methods for ModelObserver
    
    * Module with base elements.
    
    * Tests added.
    
    * Graphode, GraphElemennt, GraphConnection, Meta separated.
    
    * New constructor added
    
    * model proxy implementation in progress
    
    * return value for most methods is bool now.
    include fix.
    implementation in progress.
    
    * implementation in progress
    
    * Meta modified
    
    * Implementation in progress. Still
    
    * rearanged methods
    
    * new fields added
    
    * onPathFound event added
    
    * const specifier removed. onPathFound changed. Subscriber now is not const
    
    * role field added
    
    * Compiles now. Except tests
    
    * Utils updated
    
    * Search first implementation dijkstra.
    
    * cmake Model version. No QT
    
    * Search v1 with tests
    
    * GraphInterface tests.
    GraphConnection constructor.
    onPathFound event changed
    
    * Meta updated
    
    * ModelInterface tests updated.
    GraphInterface fix
    ModelInterface methods returns true on success now.
    
    * cmake updated
    
    * UML added
    
    * onConnectionRemoved fix
    
    * ci added
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * tinny refactoring
    
    * cmake
    
    * cmake 3.13
    
    * cmake 3.13
    
    * search
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * boost removed
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci

--- Model/model_tests.cpp
+++ Model/model_tests.cpp
@@ -1,10 +1,42 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "include/GraphInterface/GraphInterface.h"
 #include <GraphNode/GraphNode.h>
 #include "include/ModelInterface/ModelInterface.h"
 #include <include/Search/Search.h>
 #include <GraphConnection/GraphConnection.h> // ConnectionParams
 using namespace Polaris;
+
+//class MockSubScriber: public ModelSubscriber
+//{
+//    MOCK_METHOD(
+//            void, onMetaAdded,( const Meta & meta ), ()
+//    );
+//    MOCK_METHOD(
+//            void, onMetaChanged, ( const Meta & meta ), ()
+//    );
+//    MOCK_METHOD(
+//            void, onMetaRemoved, ( const Meta & meta ), ()
+//    );
+//    MOCK_METHOD(
+//            void, onConnectionAdded,( const GraphConnection & connection ), ()
+//    );
+//    MOCK_METHOD(
+//            void, onConnectionRemoved, ( const GraphConnection & connection ), ()
+//    );
+//    MOCK_METHOD(
+//            void, onPathFound,(
+//            std::vector< Meta > & path,
+//            std::vector< GraphConnection > & connections ), ()
+//    );
+//    MOCK_METHOD(
+//            void, onNodeAdded,( const GraphNode & node ), ()
+//    );
+//    MOCK_METHOD(
+//            void, onNodeRemoved, ( const GraphNode & node ), ()
+//    );
+//};
+
 TEST( GraphInterfaceTest, AddNodesHandle )
 {
     GraphInterface g;
