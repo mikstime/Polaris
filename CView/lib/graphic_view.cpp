0e9310d3a7603a4e389640a92a839d0275e0e269
Куклин С. И <akiraq@yandex.ru>
2019-12-26 21:13:35 +0300

    bug fix

--- CView/lib/graphic_view.cpp
+++ CView/lib/graphic_view.cpp
@@ -36,7 +36,7 @@ void GraphicView::ChangeRoom(const Meta & meta )
 {
     graph_parser_->OnRoomChanged(meta);
     renderer_->SetFloor( meta.floor );
-//    item_controller_->ResetEditing();
+    item_controller_->ResetEditing();
 }
 
 void GraphicView::AddRoom(const Meta & meta )
@@ -61,13 +61,13 @@ void GraphicView::RemoveConnection(const GraphConnection & connection )
 
 bool GraphicView::FloorUp() 
 {
-    item_controller_->ResetEditing();
+//    item_controller_->ResetEditing();
     return renderer_->FloorUp();
 }
 
 bool GraphicView::FloorDown()
 {
-    item_controller_->ResetEditing();
+//    item_controller_->ResetEditing();
     return renderer_->FloorDown();
 }
 
